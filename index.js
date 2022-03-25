6f55a38307fd7f23d0a2dbaad3884199c83ab7ee
juneboom <jss410@scarletmail.rutgers.edu>
2022-03-24 21:25:22 -0400

    feat: add advice generator

---
+++ index.js
@@ -0,0 +1,33 @@
+const adviceURL = 'https://api.adviceslip.com/advice';
+const cardText = document.querySelector('.card-text');
+
+const printAdvice = async() => {
+    let { slip } = await getData(adviceURL);
+    cardText.innerHTML = `
+    <h1>Advice #${slip.id}</h1>
+    <p>${slip.advice}</p>
+    `
+};
+
+const getData = async(url) => {
+    let data = await fetch(url)
+        .then((response) => {
+            if (!response.ok)
+                throw new Error(`Status Code: ${response.status}`);
+
+            return response.json();
+        })
+        .catch((err) => {
+            console.log('Something went wrong with fetch');
+            console.log(err);
+        });
+    return data;
+};
+
+printAdvice();
+
+const button = document.getElementById('button');
+button.addEventListener("click", () => {
+    console.log('On button press:');
+    printAdvice();
+});
